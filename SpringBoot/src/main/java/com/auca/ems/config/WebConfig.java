62f9c3ea4d957ebcb4127661306be9b073c3a81a
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2025-04-27 13:30:10 +0200

    Add: Enhance Security FIxes

--- SpringBoot/src/main/java/com/auca/ems/config/WebConfig.java
+++ SpringBoot/src/main/java/com/auca/ems/config/WebConfig.java
@@ -1,17 +1,30 @@
 package com.auca.ems.config;
 
-import com.auca.ems.interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     
+    @Value("${file.upload-dir:uploads}")
+    private String uploadDir;
+    
+    @Autowired
+    private RequestCounterInterceptor requestCounterInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor())
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login", "/register", "/css/**", "/js/**");
+        registry.addInterceptor(new AuthInterceptor());
+        registry.addInterceptor(requestCounterInterceptor);
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadDir + "/");
     }
 }
